35ce35821ff388d909f85cd9a0b13534d8908357
신성민 <dureotkd123@naver.com>
2022-10-19 21:46:01 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
+import axios from "axios";
+
+import "./App.css";
 function App() {
   const [products, setProducts] = React.useState([
     {
@@ -29,6 +31,20 @@ function App() {
     image: null,
   });
 
+  const 서버에있는장바구니가져오기 = async () => {
+    await axios({
+      url: "http://localhost:4000/myCart",
+    })
+      .then((response) => {
+        // setMyCart();
+      })
+      .catch((e) => {});
+  };
+
+  React.useEffect(() => {
+    서버에있는장바구니가져오기();
+  }, []);
+
   React.useEffect(() => {
     window.addEventListener("click", (event) => {
       if (event.target.className === "modal-bg") {
@@ -40,8 +56,44 @@ function App() {
     });
   }, []);
 
+  // React => useMemo
+  const 총금액 = React.useMemo(() => {
+    let 금액 = 0;
+
+    myCart.forEach((item) => {
+      금액 += parseInt(item.price);
+    });
+
+    return 금액;
+  }, [myCart]);
+
+  const 서버요청테스트 = async () => {
+    /**
+     * 동기 : 위에서부터 아래로 실행
+     * 비동기 : 알수없음
+     *
+     * 4초 이상 걸림
+     */
+    await axios({
+      method: "get",
+      dataType: "json",
+      url: "http://localhost:4000/test",
+      params: {
+        age: 30,
+        name: "SungMin",
+      },
+    })
+      .then((response) => {})
+      .catch((e) => {
+        console.log("네트워크 요청 에러..", e);
+      });
+  };
+
   return (
     <div className="App">
+      <button onClick={서버요청테스트} style={{ padding: 100 }}>
+        서버 요청 테스트 !!
+      </button>
       <div className="wrapper">
         <div className="screen -left">
           <img
@@ -80,6 +132,18 @@ function App() {
                               // myCart 배열에 똑같은 상품이 있는지 확인
                               // 똑같은 상품있으면 push X
                               const cloneMyCart = [...myCart];
+
+                              const 이미가지고있는상품 = cloneMyCart.find(
+                                (myItem) => {
+                                  return myItem.name === item.name;
+                                }
+                              );
+
+                              if (이미가지고있는상품) {
+                                alert("이미 해당 상품을 가지고 있습니다");
+                                return;
+                              }
+
                               cloneMyCart.push(item);
                               setMyCart(cloneMyCart);
                             }}
@@ -126,8 +190,18 @@ function App() {
                         <div className="description">{item.descrition}</div>
                         <div className="bottom-area">
                           <div className="price">{item.price}</div>
-                          <div className="button">
-                            <p>ADD TO CART</p>
+                          <div
+                            className="button"
+                            onClick={() => {
+                              const cloneMyCart = [...myCart];
+                              const newMyCart = cloneMyCart.filter((myItem) => {
+                                return myItem.name !== item.name;
+                              });
+                              setMyCart(newMyCart);
+                            }}
+                            style={{ backgroundColor: "red", color: "#fff" }}
+                          >
+                            <p>REMOVE</p>
                           </div>
                         </div>
                       </div>
@@ -151,6 +225,7 @@ function App() {
             )}
           </div>
         </div>
+        <h3>Total Cost : {총금액}</h3>
       </div>
 
       {showModal.show && (
